0a8d05d8dcf9e217b7c79d54cd5a79449ce441a6
George <jora.grintsevich@gmail.com>
2022-05-27 12:51:57 +0500

    skip

--- Sorting Array/Program.cs
+++ Sorting Array/Program.cs
@@ -10,32 +10,8 @@ namespace Sorting_Array
     {
         static void Main(string[] args)
         {
-            #region Test
-            int[] InsertionSortArray = new int[] { 254, 12, 10, 2, 1, 8, 3, 6, 7, 50, 78, 71, 22 };
-            //InsertionSort(InsertionSortArray);
-
-            int[] BubbleSortArray = new int[] { 254, 12, 10, 2, 1, 8, 3, 6, 7, 50, 78, 71, 22 };
-            BubbleSort(BubbleSortArray);
-
-            //object searchedint =  Search1.BinarySearchIterative(BubbleSortArray,12);
-            //var data = Array.BinarySearch(BubbleSortArray, 22);
-
-            //int number =  TestClass.Sum(new int[] { 2, 4, 6 });
-            //int count = TestClass.Count(new int[] { 2, 4, 6 });
-
-            //int maxValue = Recursion.MaxValueInArray(new int[] { 2, 4, 1000, 7, 8,10,11,100 });
-            string value = Recursion.ShowAllNumbers(168);
-            #endregion
-
-            #region Instance
-            //var db = SingletonDatabase.Instance;
-            // var city = "Tokyo";
-            //Console.WriteLine($"{city}" + " " + db.GetPooulation(city));
-
-            #endregion
-
             #region Algorithm 
-            #region GreedyAlgorithm
+            #region KnapsackProblem
             List<Product> productList = new List<Product>
             {
                 new Product{Name = "Гитара", Price = 1500, Weight = 1},
@@ -54,12 +30,32 @@ namespace Sorting_Array
             backpackWeightQuestion.DynamicAnswer();
             #endregion
             #endregion
+            
+            #region Test
+            //int[] InsertionSortArray = new int[] { 254, 12, 10, 2, 1, 8, 3, 6, 7, 50, 78, 71, 22 };
+            ////InsertionSort(InsertionSortArray);
 
+            //int[] BubbleSortArray = new int[] { 254, 12, 10, 2, 1, 8, 3, 6, 7, 50, 78, 71, 22 };
+            //BubbleSort(BubbleSortArray);
+
+            //object searchedint =  Search1.BinarySearchIterative(BubbleSortArray,12);
+            //var data = Array.BinarySearch(BubbleSortArray, 22);
+
+            //int number =  TestClass.Sum(new int[] { 2, 4, 6 });
+            //int count = TestClass.Count(new int[] { 2, 4, 6 });
+
+            //int maxValue = Recursion.MaxValueInArray(new int[] { 2, 4, 1000, 7, 8,10,11,100 });
+            string value = Recursion.ShowAllNumbers(168);
+            #endregion
+
+            #region Instance
+            //var db = SingletonDatabase.Instance;
+            // var city = "Tokyo";
+            //Console.WriteLine($"{city}" + " " + db.GetPooulation(city));
+
+            #endregion
 
             Console.ReadLine(); 
         }
-
-        
-
     }
 }
